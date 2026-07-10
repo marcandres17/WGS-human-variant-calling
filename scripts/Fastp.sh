2142081ce9e6b523627f5d49d217f76f1e851c46
Marc Andrés Blasco <marcandres17@gmail.com>
2026-07-10 15:51:13 +0200

    Rename Fastp.sh to 3.Fastp.sh

--- scripts/Fastp.sh
+++
@@ -1,3 +0,0 @@
-
-#Trimm your reads using FastP using the next command:
-fastp -i SRR062634_1.fastq.gz -I SRR062634_2.fastq.gz -o out_1.clean.fq.gz -O out_2.cleanfq.gz --detect_adapter_for_pe --trim_poly_x --trim_poly_g --cut_front 20 --cut_tail 20 --cut_mean_quality 20 -h out_FastP.html
